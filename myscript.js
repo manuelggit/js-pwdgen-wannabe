a96fecd28d9a685b744ef17d58275008f408181c
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-16 14:25:11 +0200

    Aggiunta frase introduttiva

--- myscript.js
+++ myscript.js
@@ -5,3 +5,5 @@ var cognome = prompt('Scrivi il tuo cognome')
 var colore = prompt('Scrivi il tuo colore preferito')
 
 document.getElementById('password').innerHTML= nome + cognome + colore + '19'
+
+document.getElementById('password').innerHTML= 'Ciao ' + nome + ', la tua password è ' + nome + cognome + colore + '19'
